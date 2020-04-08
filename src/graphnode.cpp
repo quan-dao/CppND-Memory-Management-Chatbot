dc907f70660376ded2023e37182456367a775f95
quan-dao <minh-quan.dao@eleves.ec-nantes.fr>
2020-04-08 21:01:42 +0200

    Task 5-Revised: Simplify passing chatbot

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -43,15 +43,13 @@ void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
 ////
 void GraphNode::MoveChatbotHere(ChatBot chatbot)
 {
-    _chatBot.reset(new ChatBot());
-    *_chatBot = std::move(chatbot);
-    _chatBot->SetCurrentNode(this);
+    _chatBot = std::move(chatbot);
+    _chatBot.SetCurrentNode(this);
 }
 
 void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
 {
-    newNode->MoveChatbotHere(std::move(*_chatBot));
-    _chatBot.reset();
+    newNode->MoveChatbotHere(std::move(_chatBot));
     // _chatBot = nullptr; // invalidate pointer at source
 }
 ////
